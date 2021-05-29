f35607bb162b34f8a454c7a69611eaf93aab1e0c
Codeclan Student <student@Codeclans-MacBook-Pro.local>
2021-05-29 10:03:08 +0100

    started combining python with html

--- controllers/products_controller.py
+++ controllers/products_controller.py
@@ -5,12 +5,14 @@ from models.product import Product
 
 products_blueprint = Blueprint("products", __name__)
 
-@products_blueprint.route("/inventory")
-def stock():
-    stock = product_repository.select_all()
-    return render_template("products/inventory.html", all_stock=stock)
+@products_blueprint.route("/products")
+def product():
+    products = product_repository.select_all()
+    return render_template("products/stock.html", all_products=products)
+
+
+
 
 @products_blueprint.route("/orders")
 def recent_orders():
-    
     return render_template("products/orders.html")
