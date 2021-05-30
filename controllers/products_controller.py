9340ea396135e269c914c4a21d9b9add366e66c8
Codeclan Student <student@Codeclans-MacBook-Pro.local>
2021-05-30 15:01:53 +0100

    delete button working

--- controllers/products_controller.py
+++ controllers/products_controller.py
@@ -10,7 +10,29 @@ def product():
     products = product_repository.select_all()
     return render_template("products/stock.html", all_products=products)
 
+@products_blueprint.route("/product/<id>/edit", methods=["GET"])
+def edit_product(id):
+    product = product_repository.select(id)
+    manufacturers = manufacturer_repository.select_all()
+    return render_template('products/edit.html', product=product, all_manufacturers=manufacturers)
 
+@products_blueprint.route("/products/<id>", methods=["POST"])
+def update_product(id):
+    name = request.form['name']
+    description = request.form['description']
+    qty = request.form['qty']
+    buy = request.form['buy']
+    sell = request.form['sell']
+    # manufacturer_id = request.form['manufacturer']
+    manufacturer = manufacturer_repository.select(id)
+    product = Product(name,manufacturer,description,qty,buy,sell)
+    product_repository.update(product)
+    return redirect('/products')
+
+@products_blueprint.route("/products/<id>/delete", methods=["POST"])
+def delete_product(id):
+    product_repository.delete(id)
+    return redirect('/products')
 
 
 @products_blueprint.route("/orders")
